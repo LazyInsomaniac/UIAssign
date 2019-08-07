971ce3c0e3c7ef44b66455ab572b286826b10422
LazyInsomaniac <sambhavsharma2@gmail.com>
2019-08-07 17:31:07 +0530

    7Aug19

---
+++ Angular JS/test.js
@@ -0,0 +1,5 @@
+let tester =()=>{
+    console.log('Hi,This is Sam');
+    document.write('Hi,This is Sam');
+}
+tester();
---
+++ Angular JS/test1.js
@@ -0,0 +1,6 @@
+'use-strict';
+var tester=function tester(){
+    console.log('Hi this is Sam')
+    document.write('Hi,This is Sam');
+};
+tester();
